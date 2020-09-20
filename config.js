18aec34e3a48b48e14a375c63124951d463c901f
honeybaey <honeybaey@gmail.com>
2020-09-20 16:54:43 +0300

    changed the url

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const isProduction = process.env.NODE_ENV === "production";
 
 module.exports = {
-  baseUrl: isProduction ? "/purplelanding/" : "../"
+  baseUrl: isProduction ? "/purpleagency/" : "../"
 };
